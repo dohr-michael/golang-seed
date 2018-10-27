eca674c7387c9873f642261db4be0ba8728ac7e5
Michaël Dohr <dohr.michael@gmail.com>
2018-10-27 09:31:55 +0200

    Update start.go

--- cmd/start.go
+++ cmd/start.go
@@ -2,14 +2,22 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
-	"fmt"
+        "net/http"
+	"log"
 )
 
 var startCmd = &cobra.Command{
 	Use: "start",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("Start :)")
-		return nil
+		log.Println("Start golang-seed")
+		server := http.NewServeMux()
+
+		server.HandleFunc("/@/health", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			w.Write([]byte(`{"status": "OK"}`))
+		})
+
+		return http.ListenAndServe(":8080", server)
 	},
 }
 
