f4e3d6f2e18a60a89faee41a0d635962c9e0979c
Michaël Dohr <dohr.michael@gmail.com>
2019-03-09 14:00:52 +0100

    Change Run by RunV and add gin run command

--- magefile.go
+++ magefile.go
@@ -10,30 +10,35 @@ func init() {
 }
 
 func downloadDeps() error {
-	return sh.Run("go", "mod", "download")
+	return sh.RunV("go", "mod", "download")
 }
 
 func generate() error {
-	return sh.Run("go", "generate", "./...")
+	return sh.RunV("go", "generate", "./...")
+}
+
+func Run() error {
+	mg.Deps(downloadDeps, generate)
+	return sh.RunV("gin", "--appPort", "8080", "--buildArgs", "main.go", "-i", "run", "start")
 }
 
 func Test() error {
 	mg.Deps(downloadDeps, generate)
-	return sh.Run("go", "test", "./...")
+	return sh.RunV("go", "test", "./...")
 }
 
 func Build() error {
 	mg.Deps(Test)
-	return sh.Run("go", "build", "./...")
+	return sh.RunV("go", "build", "./...")
 }
 
 
 func Snapshot() error {
 	mg.Deps(Test)
-	return sh.Run("goreleaser", "--rm-dist", "--snapshot")
+	return sh.RunV("goreleaser", "--rm-dist", "--snapshot")
 }
 
 func Release() error {
 	mg.Deps(Test)
-	return sh.Run("goreleaser", "--rm-dist")
+	return sh.RunV("goreleaser", "--rm-dist")
 }
